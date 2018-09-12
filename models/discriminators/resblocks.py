69f32dfb5ec14c658db913f34df1a4f25d67cb18
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2018-09-12 23:40:06 +0900

    make ReflectionPad2d available (#12)
    * make ReflectionPad2d available
    
    * add training configurations

--- models/discriminators/resblocks.py
+++ models/discriminators/resblocks.py
@@ -1,6 +1,5 @@
 import math
 
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import init
@@ -9,8 +8,24 @@ from torch.nn import utils
 
 class Block(nn.Module):
 
+    """Residual block of Discriminator.
+
+    Args:
+        in_ch (int): Number of channels of input.
+        out_ch (int): Number of channels of output.
+        h_ch (int, optional): Number of channels of hidden
+        ksize (int, optional): Kernel size. (Default: 3)
+        pad (int, optional): width of padding. (Default: 1)
+        padding (str, optional): Padding method. (Default: zero)
+        activation (callable, optional): Default: ReLU
+        upsample (bool, optional): Default: False
+        num_classes (int, optional):
+            Argument for categorical conditional batch normalization
+
+    """
+
     def __init__(self, in_ch, out_ch, h_ch=None, ksize=3, pad=1,
-                 activation=F.relu, downsample=False):
+                 padding='zero', activation=F.relu, downsample=False):
         super(Block, self).__init__()
 
         self.activation = activation
@@ -20,12 +35,19 @@ class Block(nn.Module):
         if h_ch is None:
             h_ch = out_ch
 
-        self.c1 = utils.spectral_norm(nn.Conv2d(in_ch, h_ch, ksize, 1, pad))
-        self.c2 = utils.spectral_norm(nn.Conv2d(h_ch, out_ch, ksize, 1, pad))
+        self.c1 = utils.spectral_norm(nn.Conv2d(in_ch, h_ch, ksize, 1, 0))
+        self.c2 = utils.spectral_norm(nn.Conv2d(h_ch, out_ch, ksize, 1, 0))
         if self.learnable_sc:
-            self.c_sc = utils.spectral_norm(nn.Conv2d(in_ch, out_ch, ksize, 1, 0))
+            self.c_sc = utils.spectral_norm(
+                nn.Conv2d(in_ch, out_ch, ksize, 1, 0)
+            )
 
         self._initialize()
+
+        if padding == 'reflection':
+            self.padding = nn.ReflectionPad2d(pad)
+        else:
+            self.padding = nn.ZeroPad2d(pad)
 
     def _initialize(self):
         init.xavier_uniform_(self.c1.weight.data, math.sqrt(2))
@@ -40,12 +62,12 @@ class Block(nn.Module):
         if self.learnable_sc:
             x = self.c_sc(x)
         if self.downsample:
-            return F.avg_pool2d(x, 2, padding=1)
+            return F.avg_pool2d(self.padding(x), 2, padding=0)
         return x
 
     def residual(self, x):
-        h = self.c1(self.activation(x))
-        h = self.c2(self.activation(h))
+        h = self.c1(self.padding(self.activation(x)))
+        h = self.c2(self.padding(self.activation(h)))
         if self.downsample:
             h = F.avg_pool2d(h, 2)
         return h
@@ -53,15 +75,20 @@ class Block(nn.Module):
 
 class OptimizedBlock(nn.Module):
 
-    def __init__(self, in_ch, out_ch, ksize=3, pad=1, activation=F.relu):
+    def __init__(self, in_ch, out_ch, ksize=3, pad=1, padding='zero', activation=F.relu):
         super(OptimizedBlock, self).__init__()
         self.activation = activation
 
-        self.c1 = utils.spectral_norm(nn.Conv2d(in_ch, out_ch, ksize, 1, pad))
-        self.c2 = utils.spectral_norm(nn.Conv2d(out_ch, out_ch, ksize, 1, pad))
+        self.c1 = utils.spectral_norm(nn.Conv2d(in_ch, out_ch, ksize, 1, 0))
+        self.c2 = utils.spectral_norm(nn.Conv2d(out_ch, out_ch, ksize, 1, 0))
         self.c_sc = utils.spectral_norm(nn.Conv2d(in_ch, out_ch, 1, 1, 0))
 
         self._initialize()
+
+        if padding == 'reflection':
+            self.padding = nn.ReflectionPad2d(pad)
+        else:
+            self.padding = nn.ZeroPad2d(pad)
 
     def _initialize(self):
         init.xavier_uniform_(self.c1.weight.data, math.sqrt(2))
@@ -75,5 +102,5 @@ class OptimizedBlock(nn.Module):
         return self.c_sc(F.avg_pool2d(x, 2))
 
     def residual(self, x):
-        h = self.activation(self.c1(x))
-        return F.avg_pool2d(self.c2(h), 2)
+        h = self.activation(self.c1(self.padding(x)))
+        return F.avg_pool2d(self.c2(self.padding(h)), 2)
--- models/generators/resnet.py
+++ models/generators/resnet.py
@@ -9,8 +9,9 @@ from models.generators.resblocks import Block
 class ResNetGenerator(nn.Module):
     """Generator generates 128x128."""
 
-    def __init__(self, num_features=64, dim_z=128, bottom_width=4,
-                 activation=F.relu, num_classes=0, distribution='normal'):
+    def __init__(self, num_features=64, dim_z=128, padding='zero',
+                 bottom_width=4, activation=F.relu, num_classes=0,
+                 distribution='normal'):
         super(ResNetGenerator, self).__init__()
         self.num_features = num_features
         self.dim_z = dim_z
@@ -22,22 +23,27 @@ class ResNetGenerator(nn.Module):
         self.l1 = nn.Linear(dim_z, 16 * num_features * bottom_width ** 2)
 
         self.block2 = Block(num_features * 16, num_features * 16,
-                            activation=activation, upsample=True,
-                            num_classes=num_classes)
+                            padding=padding, activation=activation,
+                            upsample=True, num_classes=num_classes)
         self.block3 = Block(num_features * 16, num_features * 8,
-                            activation=activation, upsample=True,
-                            num_classes=num_classes)
+                            padding=padding, activation=activation,
+                            upsample=True, num_classes=num_classes)
         self.block4 = Block(num_features * 8, num_features * 4,
-                            activation=activation, upsample=True,
-                            num_classes=num_classes)
+                            padding=padding, activation=activation,
+                            upsample=True, num_classes=num_classes)
         self.block5 = Block(num_features * 4, num_features * 2,
-                            activation=activation, upsample=True,
-                            num_classes=num_classes)
+                            padding=padding, activation=activation,
+                            upsample=True, num_classes=num_classes)
         self.block6 = Block(num_features * 2, num_features,
-                            activation=activation, upsample=True,
-                            num_classes=num_classes)
+                            padding=padding, activation=activation,
+                            upsample=True, num_classes=num_classes)
         self.b7 = nn.BatchNorm2d(num_features)
-        self.conv7 = nn.Conv2d(num_features, 3, 1, 1)
+        self.conv7 = nn.Conv2d(num_features, 3, 1, 1, 0)
+
+        if padding == 'reflection':
+            self.padding = nn.ReflectionPad2d(1)
+        else:
+            self.padding = nn.ZeroPad2d(1)
 
     def _initialize(self):
         init.xavier_uniform_(self.l1.weight.tensor)
@@ -48,4 +54,5 @@ class ResNetGenerator(nn.Module):
         for i in [2, 3, 4, 5, 6]:
             h = getattr(self, 'block{}'.format(i))(h, y, **kwargs)
         h = self.activation(self.b7(h))
-        return torch.tanh(self.conv7(h))
+        h = self.conv7(self.padding(h))
+        return torch.tanh(h)
